86c351bd0d72884e407ae912d72bc0576dd2deae
Shubham Malhotra <shubhammalhotra311@gmail.com>
2024-10-11 15:32:21 -0400

    Updated Info

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function handleScrollAnimation() {
+  const elements = document.querySelectorAll('.fade-in');
+  
+  elements.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+    const elementVisible = 150;
+    
+    if (elementTop < window.innerHeight - elementVisible) {
+      element.classList.add('appear');
+    } else {
+      element.classList.remove('appear');
+    }
+  });
+}
+
+window.addEventListener('scroll', handleScrollAnimation);
+window.addEventListener('load', handleScrollAnimation);
